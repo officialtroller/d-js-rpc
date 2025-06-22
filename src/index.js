68a05d7ed742da2c543f14ad775348ebc1226fc3
officialtroller <127138313+officialtroller@users.noreply.github.com>
2025-06-22 15:20:40 +0200

    Add Activity Types as module

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 const util = require('./util');
 
 module.exports = {
-  Client: require('./client'),
-  register(id) {
-    return util.register(`discord-${id}`);
-  },
+    Client: require('./client'),
+    register(id) {
+        return util.register(`discord-${id}`);
+    },
+    ActivityTypes: require('./ActivityTypes'),
 };
